f856a9063e8daf50e12d820c0f40722b023589cd
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-25 23:10:15 +0530

    apis request

--- request.js
+++ request.js
@@ -1,6 +1,9 @@
 
 const redux = require("redux")
 const createStore = redux.createStore
+const axios = require("axios")
+const thunkMiddleware = require("redux-thunk").default
+const applyMiddleware = redux.applyMiddleware
 
 // api => get => data 
 
@@ -62,4 +65,46 @@ const reducer = (state=initialState, action) => {
     }
 }
 
-const store = createStore(reducer)
+
+// 1) return function  => from action creators
+// 2) impure function => async call there
+
+const fetchProducts = () => {
+      return function(dispatch){
+           dispatch(fetchProductsRequest())
+
+             axios.get("https://fakestoreapi.com/products")
+             .then(response =>{
+                    // console.log(response.data) 
+                    let titles = response.data.map(product => product.title)
+                    dispatch(fetchProductsSuccess(titles))
+             })
+             .catch( err =>
+                dispatch(fetchProductsFailure(err.message))
+                )
+      }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+
+store.subscribe(()=> console.log(store.getState()))
+
+store.dispatch(fetchProducts())
+
+
+// pure function: 
+
+
+// function sum(a,b){
+//     return a+b
+// }
+
+// sum(10,20) // 30
+
+// let c = 0
+// function count(a){
+//     return a + c++
+// }
+
+// count(0) // 0 
+// count(0) // 1
