46a9c583157aa0f545e74508daf60d14b59bbf27
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-25 20:56:00 +0530

    added a action and bind action

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // import redux from "redux" // es6 module
 const redux = require("redux") // common js module
+const bindActionCreators = redux.bindActionCreators  // helper function
 
 // create store: 
 const createStore = redux.createStore
@@ -8,6 +9,7 @@ const createStore = redux.createStore
 
 // create const variables for all actions types
 const CUSTOMER_COUNT = "CUSTOMER_COUNT"
+const CUSTOMER_LEFT = "CUSTOMER_LEFT"
 
 
 // action: 
@@ -18,12 +20,19 @@ const CUSTOMER_COUNT = "CUSTOMER_COUNT"
 
   // action creator => simple function that returns an action
 
-  function  customerCount(){ //  whenver a customer joins, we will call this function
+function  customerCount(){ //  whenver a customer joins, we will call this function
        return {
         type: CUSTOMER_COUNT,
         customers:  1
       }
-  }
+}
+
+function customerLeft(count){
+    return{
+        type: CUSTOMER_LEFT,
+        customers: count
+    }
+}
 
   // now redcuers: 
 
@@ -39,6 +48,11 @@ const reducer1 = (state=initialState, action) =>{
                 return{
                     ...state,
                     number_of_customers: state.number_of_customers + action.customers
+                }
+        case CUSTOMER_LEFT:
+                return{
+                    ...state,
+                    number_of_customers: state.number_of_customers - action.customers
                 }
         default:
             return state
@@ -62,8 +76,14 @@ console.log("Initial state", store.getState()) // intital state => 1000
 
 store.subscribe((()=> console.log("Updated state", store.getState())))
 
-store.dispatch(customerCount())
-store.dispatch(customerCount())
-store.dispatch(customerCount())
-store.dispatch(customerCount())
-store.dispatch(customerCount())
+// store.dispatch(customerCount())
+// store.dispatch(customerCount())
+// store.dispatch(customerCount())
+// store.dispatch(customerLeft(10))
+
+const actions =  bindActionCreators({customerCount, customerLeft}, store.dispatch )
+
+actions.customerCount()
+actions.customerCount()
+actions.customerCount()
+actions.customerLeft(10)
